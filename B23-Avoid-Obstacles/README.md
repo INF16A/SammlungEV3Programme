3988675b13b875d898621ab11a9fa132d7479303
Carsten Hagemann <carsten@chagemann.de>
2019-05-02 13:26:07 +0200

    Add newer version, readme and diagrams

---
+++ B23-Avoid-Obstacles/README.md
@@ -0,0 +1,5 @@
+# Swarm Lab: Avoid Obstacles
+
+The mindstorm roboter drives around the right side of an obstacle, and continues its way on the other side of the obstacle:
+
+![diagram](diagram.png)
--- B23-Avoid-Obstacles/main.py
+++ B23-Avoid-Obstacles/main.py
@@ -10,14 +10,11 @@ from pybricks.robotics import DriveBase
 
 obstacle_sensor = UltrasonicSensor(Port.S3)
 gyro_sensor_front = GyroSensor(Port.S4)
-# gyro_sensor_side = GyroSensor(Port.S2)
 right_motor = Motor(Port.C)
 left_motor = Motor(Port.B)
 wheel_diameter = 56
 axle_track = 114
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-
-rotateAngle = 90
 
 def start():
     while True:
@@ -26,45 +23,43 @@ def start():
             robot.stop()
             obstacle_avoidance()
 
-def obstacle_avoidance(step = 2000, speed = 200, min_dist = 250):
+def obstacle_avoidance(step = 2000, speed = 200, min_dist = 250, rotation_angle = 90):
     print('avoid')
     distance = 0
 
-    rotate(rotateAngle)
-    distance += drive_past(step, speed, min_dist)
+    rotate(rotation_angle)
+    distance += drive_past(step, speed, min_dist, rotation_angle)
     print('bottom finished')
 
-    drive_past(step, speed, min_dist)
+    drive_past(step, speed, min_dist, rotation_angle)
     print('right finished')
 
-    robot.drive_time(speed, 0, distance)
-    rotate(rotateAngle)
+    robot.drive_time(speed, 0, distance, rotation_angle)
+    rotate(rotation_angle)
     print('top finished')
 
-def drive_past(step, speed, min_dist):
+def drive_past(step, speed, min_dist, rotation_angle):
     past_object = False
     distance = 0
 
     while not past_object:
         distance = distance + step
         robot.drive_time(speed, 0, step)
-        rotate(-rotateAngle)
+        rotate(-rotation_angle)
         past_object = obstacle_sensor.distance() > min_dist
         if not past_object:
-            rotate(rotateAngle)
+            rotate(rotation_angle)
 
     return distance
 
 def rotate(angle):
-    if angle > 0:
-        angle -= 10
-
     sign = 1 if angle >= 0 else -1
     gyro_sensor_front.reset_angle(0)
+
     while sign * gyro_sensor_front.angle() <= sign * angle:
         print(gyro_sensor_front.angle())
         gyro_angle = gyro_sensor_front.angle()
-        if (gyro_angle * sign > 50): # hier fehlen die signs
+        if (gyro_angle * sign > 50):
             robot.drive_time(0, 50 * sign, 70)
         else:
             robot.drive_time(0, 150 * sign, 70)
